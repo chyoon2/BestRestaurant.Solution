2999d550c5121b6be7c2860a60928e40e4856cf7
Tyson Lackey <lackeyt90@gmail.com>
2020-07-29 18:44:06 -0700

    add functionality to restaurant model to handle default database values
    Co-authored-by: Ben White <bwhite2140@outlook.com>
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
 
 namespace BestRestaurant.Models
 {
@@ -8,8 +10,11 @@ namespace BestRestaurant.Models
     public int RestaurantId { get; set; }
     public int CuisineId { get; set; }
     public string Name { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string Location { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string PhoneNumber { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Rating { get; set; }
     public virtual Cuisine Cuisine { get; set; }
   }
